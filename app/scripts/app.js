77a3b08b3b640aaec89529fcff3c237bd9930510
alphaadidas <github@emeraldworks.com>
2015-04-09 22:04:47 -0700

    stubs

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -29,6 +29,10 @@ angular
         templateUrl: 'views/about.html',
         controller: 'AboutCtrl'
       })
+      .when('/math', {
+        templateUrl: 'views/math.html',
+        controller: 'MathCtrl'
+      })
       .otherwise({
         redirectTo: '/'
       });
---
+++ app/scripts/controllers/math.js
@@ -0,0 +1,15 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name workspaceTechnicityApp.controller:MathCtrl
+ * @description
+ * # AboutCtrl
+ * Controller of the workspaceTechnicityApp
+ */
+angular.module('workspaceTechnicityApp')
+  .controller('MathCtrl', function ($scope) {
+    $scope.numbers = [1,2,3,4,5,6,7,8,9,10,11,12,13,14,15];
+
+
+  });
